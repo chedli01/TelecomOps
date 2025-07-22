47a9e9af579fcdb0b94d2d94b8714572fa0c784a
chedli01 <chedli.masmoudi01@gmail.com>
2025-07-22 09:45:34 +0100

    creatin User Entity

---
+++ src/main/java/com/coding/internship/user/model/User.java
@@ -0,0 +1,25 @@
+package com.coding.internship.user.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    @Column(unique = true)
+    private String email;
+}
