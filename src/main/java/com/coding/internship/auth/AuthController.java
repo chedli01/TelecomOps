a4a6f7ecb89a7304e83ff6bdbb6d37195af0fae1
chedli01 <chedli.masmoudi01@gmail.com>
2025-07-22 14:50:17 +0100

    implementing /register endpoint

--- src/main/java/com/coding/internship/auth/AuthController.java
+++ src/main/java/com/coding/internship/auth/AuthController.java
@@ -1,6 +1,11 @@
 package com.coding.internship.auth;
 
+import com.coding.internship.auth.dto.RegisterRequest;
+import com.coding.internship.auth.dto.RegisterResponse;
+import com.coding.internship.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public RegisterResponse register(@RequestBody RegisterRequest registerRequest){
+        return authService.register(registerRequest) ;
+    }
 }
--- src/main/java/com/coding/internship/auth/service/AuthService.java
+++ src/main/java/com/coding/internship/auth/service/AuthService.java
@@ -4,16 +4,38 @@ import com.coding.internship.auth.dto.AuthRequest;
 import com.coding.internship.auth.dto.AuthResponse;
 import com.coding.internship.auth.dto.RegisterRequest;
 import com.coding.internship.auth.dto.RegisterResponse;
+import com.coding.internship.security.jwt.JwtService;
+import com.coding.internship.user.admin.repository.AdminRepository;
+import com.coding.internship.user.client.model.Client;
+import com.coding.internship.user.client.repository.ClientRepository;
+import com.coding.internship.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
+    private final ClientRepository clientRepository;
+    private final AdminRepository adminRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     public RegisterResponse register(RegisterRequest registerRequest){
-
-        return null;
+        var client = Client.builder()
+                .email(registerRequest.getEmail())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
+                .address(registerRequest.getAddress())
+                .cin(registerRequest.getCin())
+                .firstName(registerRequest.getFirstname())
+                .lastName(registerRequest.getLastname())
+                .phoneNumber(registerRequest.getPhoneNumber())
+                .isStudent(registerRequest.getIsStudent())
+                .build();
+        clientRepository.save(client);
+        return RegisterResponse.builder().build();
 
 
 
