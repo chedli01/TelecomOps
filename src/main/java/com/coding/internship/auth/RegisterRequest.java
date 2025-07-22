30d93fef529a648d533d0b36ebbd5a43301fe8b6
chedli01 <chedli.masmoudi01@gmail.com>
2025-07-22 14:32:33 +0100

    create RegisterRequest and RegisterResponse

---
+++ src/main/java/com/coding/internship/auth/RegisterRequest.java
@@ -0,0 +1,24 @@
+package com.coding.internship.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    @NotBlank(message = "Firstname should not be blank")
+    private String firstname;
+    private String lastname;
+    @Email(message = "Email should be valid")
+    private String email;
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+}
---
+++ src/main/java/com/coding/internship/auth/RegisterResponse.java
@@ -0,0 +1,17 @@
+package com.coding.internship.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterResponse {
+    private String firstName;
+    private String lastName;
+    private String email;
+}
+
