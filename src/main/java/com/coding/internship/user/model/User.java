0a9edcbbc3b605ccbdc99a703ced4b71b0e709ea
chedli01 <chedli.masmoudi01@gmail.com>
2025-07-22 13:49:15 +0100

    add security dependencies + create SecurityConfigBeans

--- src/main/java/com/coding/internship/user/model/User.java
+++ src/main/java/com/coding/internship/user/model/User.java
@@ -6,6 +6,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -14,7 +19,7 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @SuperBuilder
 @Inheritance(strategy = InheritanceType.JOINED)
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
@@ -23,4 +28,19 @@ public class User {
     protected String password;
     @Column(unique = true)
     protected String email;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
 }
