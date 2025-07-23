a4e41f0178e073c249c9f66e177452f6d2fd623d
chedli01 <chedli.masmoudi01@gmail.com>
2025-07-23 09:42:38 +0100

    adding validation to AuthRequest

--- src/main/java/com/coding/internship/auth/dto/AuthRequest.java
+++ src/main/java/com/coding/internship/auth/dto/AuthRequest.java
@@ -1,5 +1,7 @@
 package com.coding.internship.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +12,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthRequest {
+    @Email(message = "this should be a valid email")
+    @NotBlank(message = "this field is required")
     private String email;
+    @NotBlank(message = "this field is required")
     private String password;
 }
