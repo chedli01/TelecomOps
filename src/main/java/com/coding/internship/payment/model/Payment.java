e45706c41bc93299434732250fbdb3e098ad2a29
chedli01 <chedli.masmoudi01@gmail.com>
2025-07-24 12:27:28 +0100

    Payment entity creation

---
+++ src/main/java/com/coding/internship/payment/model/Payment.java
@@ -0,0 +1,26 @@
+package com.coding.internship.payment.model;
+
+import com.coding.internship.payment.enums.PaymentMethod;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "payments")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String paymentNumber;
+    private LocalDateTime paymentDate;
+    private PaymentMethod paymentMethod;
+
+}
