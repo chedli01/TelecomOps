7be9f6f3e72bf259f996658f4795278166be1be9
chedli01 <chedli.masmoudi01@gmail.com>
2025-07-24 12:36:08 +0100

    Creating Order Entity

---
+++ src/main/java/com/coding/internship/order/model/Order.java
@@ -0,0 +1,22 @@
+package com.coding.internship.order.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "orders")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderNumber;
+    private String description;
+    private Double discount;
+}
