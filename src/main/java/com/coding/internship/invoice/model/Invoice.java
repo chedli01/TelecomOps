58a0bb19c4f93dfc4f967fc5145f570053a62066
chedli01 <chedli.masmoudi01@gmail.com>
2025-07-24 12:02:39 +0100

    Invoice entity

---
+++ src/main/java/com/coding/internship/invoice/model/Invoice.java
@@ -0,0 +1,33 @@
+package com.coding.internship.invoice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "invoices")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Invoice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String invoiceNumber;
+    private String description;
+    private Double total;
+    private LocalDateTime dueDate;
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
