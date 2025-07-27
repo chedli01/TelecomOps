394ea441e0034403ed1bafe4f4c1d341eb65c845
chedli01 <chedli.masmoudi01@gmail.com>
2025-07-27 14:11:58 +0100

    implementation of SubscribeToPlan

--- src/main/java/com/coding/internship/subscription/service/SubscriptionService.java
+++ src/main/java/com/coding/internship/subscription/service/SubscriptionService.java
@@ -1,17 +1,38 @@
 package com.coding.internship.subscription.service;
 
+import com.coding.internship.plan.model.Plan;
+import com.coding.internship.plan.repository.PlanRepository;
 import com.coding.internship.subscription.dto.SubscriptionDataDto;
+import com.coding.internship.subscription.enums.SubscriptionStatus;
+import com.coding.internship.subscription.model.Subscription;
 import com.coding.internship.subscription.repository.SubscriptionRepository;
+import com.coding.internship.user.client.model.Client;
+import com.coding.internship.user.client.repository.ClientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class SubscriptionService {
     private final SubscriptionRepository subscriptionRepository;
+    private final PlanRepository planRepository;
+    private final ClientRepository clientRepository;
 
     public SubscriptionDataDto subscribeToPlan(Long planId, Long clientId){
-        return null;
+        Plan plan = planRepository.findById(planId).orElseThrow();
+        Client client = clientRepository.findById(clientId).orElseThrow();
+        var subscription = Subscription.builder()
+                           .plan(plan)
+                           .client(client).startDate(LocalDateTime.now()).endDate(LocalDateTime.now().plusDays(plan.getValidityDays()))
+                .remainingData(plan.getDataQuota()).remainingCalls(plan.getCallsMinutes()).remainingSms(plan.getSmsNumber()).discount(0.0).status(SubscriptionStatus.ACTIVE)
+                .build();
+        Subscription savedSub = subscriptionRepository.save(subscription);
+        return SubscriptionDataDto.builder().id(savedSub.getId()).startDate(savedSub.getStartDate()).endDate(savedSub.getEndDate())
+                .planName(plan.getName()).planDescription(plan.getDescription()).planPrice(plan.getPrice()).totalToPay(plan.getPrice()-savedSub.getDiscount()).build();
+
+
 
     }
 
