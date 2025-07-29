90d0ea027109c82e38602d1a219dc678a4506a6b
chedli01 <chedli.masmoudi01@gmail.com>
2025-07-29 10:23:33 +0100

    fix Payment Entity

--- src/main/java/com/coding/internship/payment/model/Payment.java
+++ src/main/java/com/coding/internship/payment/model/Payment.java
@@ -22,6 +22,7 @@ public class Payment {
     private Long id;
     private String paymentNumber;
     private LocalDateTime paymentDate;
+    @Enumerated(EnumType.STRING)
     private PaymentMethod paymentMethod;
     private Double amount;
     @OneToOne
