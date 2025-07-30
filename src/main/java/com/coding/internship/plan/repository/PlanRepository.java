4c3b6ee5734515ed12d614de8a35cabe23dba419
chedli01 <chedli.masmoudi01@gmail.com>
2025-07-30 11:28:04 +0100

    getNextPlanByDataQuota query

--- src/main/java/com/coding/internship/plan/repository/PlanRepository.java
+++ src/main/java/com/coding/internship/plan/repository/PlanRepository.java
@@ -2,6 +2,10 @@ package com.coding.internship.plan.repository;
 
 import com.coding.internship.plan.model.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PlanRepository extends JpaRepository<Plan, Long> {
+    @Query(value = "SELECT * from plans p where p.data_quota > (select data_quota from plans where id=:id) order by p.data_quota asc limit 1",nativeQuery = true)
+    Plan getNextPlanByDataQuota(@Param("id") Long id);
 }
