44f1c69dd4196726ca6a9088afc1a870524d4942
chedli01 <chedli.masmoudi01@gmail.com>
2025-08-01 13:47:50 +0100

    Update Order endpoint

--- src/main/java/com/coding/internship/order/OrderController.java
+++ src/main/java/com/coding/internship/order/OrderController.java
@@ -2,16 +2,14 @@ package com.coding.internship.order;
 
 import com.coding.internship.order.dto.OrderCreateDto;
 import com.coding.internship.order.dto.OrderDataDto;
+import com.coding.internship.order.dto.OrderUpdateDto;
 import com.coding.internship.order.mapper.OrderMapper;
 import com.coding.internship.order.model.Order;
 import com.coding.internship.order.service.OrderService;
 import com.coding.internship.user.client.model.Client;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
@@ -23,5 +21,10 @@ public class OrderController {
     @PostMapping
     public OrderDataDto makeOrder(@RequestBody OrderCreateDto orderCreateDto, @AuthenticationPrincipal Client client){
         return orderMapper.mapToDto(orderService.makeOrder(orderCreateDto,client.getId())) ;
+    }
+    @PutMapping("/{id}")
+    public OrderDataDto updateOrder(@RequestBody OrderUpdateDto orderUpdateDto,@PathVariable Long id){
+        return orderMapper.mapToDto(orderService.updateOrder(id,orderUpdateDto));
+
     }
 }
