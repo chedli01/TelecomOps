1110cb52b7f5f481f003762044e8fe17fab97e9e
chedli01 <chedli.masmoudi01@gmail.com>
2025-08-05 13:54:02 +0100

    some code fixes

--- src/main/java/com/coding/internship/drools/service/DroolsService.java
+++ src/main/java/com/coding/internship/drools/service/DroolsService.java
@@ -7,7 +7,6 @@ import com.coding.internship.order.model.Order;
 import com.coding.internship.payment.model.Payment;
 import com.coding.internship.plan.model.Plan;
 import com.coding.internship.plan.service.PlanService;
-import com.coding.internship.product.model.Product;
 import com.coding.internship.product.service.SpeceficService;
 import com.coding.internship.subscription.model.Subscription;
 import com.coding.internship.user.client.model.Client;
@@ -17,8 +16,6 @@ import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @Service
@@ -32,40 +29,7 @@ public class DroolsService {
     private final PlanService planService;
 
 
-    public Product applyDiscount(Product product) {
-        KieSession kieSession = kieContainer.newKieSession("ksession-rules");
-        try {
-            kieSession.insert(product);
-            kieSession.getAgenda().getAgendaGroup("pricing").setFocus();
-            kieSession.fireAllRules();
-            return product;
-        } finally {
-            kieSession.dispose();
-        }
-    }
-//
-    public List<Product> applyDiscountForCategory(List<Product> products, String category) {
-        KieSession kieSession = kieContainer.newKieSession("ksession-rules");
 
-        try {
-            // Track modified products
-            List<Product> modifiedProducts = new ArrayList<>();
-
-            // Set global before inserting facts
-            kieSession.setGlobal("targetCategory", category);
-            kieSession.setGlobal("modifiedProducts", modifiedProducts);
-
-            // Insert all products
-            products.forEach(kieSession::insert);
-
-            // Fire rules
-            kieSession.fireAllRules();
-
-            return modifiedProducts;
-        } finally {
-            kieSession.dispose();
-        }
-    }
     public ResponseObjectDto applyOrderProcess(Order order){
         KieSession kieSession = kieContainer.newKieSession("ksession-rules");
         Subscription subscription =clientService.getActiveSub(order.getClient().getId());
@@ -175,21 +139,4 @@ public class DroolsService {
 
     }
 
-//
-//    public Product changeInDb(Product product,Long id){
-//        KieSession kieSession = kieContainer.newKieSession("ksession-rules");
-//        try {
-//            kieSession.setGlobal("productService",speceficService);
-//            kieSession.setGlobal("ProductId",id);
-//            kieSession.insert(product);
-//            kieSession.getAgenda().getAgendaGroup("renaming").setFocus();
-//            kieSession.fireAllRules();
-//            return product;
-//
-//
-//        }
-//        finally {
-//            kieSession.dispose();
-//        }
-//    }
 }
